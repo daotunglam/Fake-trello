f56c7bd91127278c745cee9fc4f9accc49381fde
daotunglam <lam90namdinh@gmail.com>
2021-11-04 16:01:26 +0100

    make complete new

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-/**
- * SOME PROBLEMS I'VE NOT FOUND THE SOLUTION:
- * 
- * - Is there toggle for style.display: none; ?
- * 
- * - In order to make titles text having <br> and looking like in textaria
- *    i've tried title.replaceAll('\n', `<br>`) , but it doesn't work
- *    => solution put title.replaceAll('\n', `<br>`) into the task
- * 
- * - how to make .addingField and .list not overflow the bottom of screen?
- * 
- * - when many btn saveEditedTask opened, they'll be all hidden when only 1 of them clicked.
- *    How to fix this mistake?
- * 
- * - description has only 2rem height. how to change that?
- */
 
+/*==========================
+FOR ALL BODY TAGS 
+===========================*/
 
-
-
-
-
-/*== FOR ALL BODY TAGS =======================================*/
+let allColumns = [];
+let allTasks = [];
 
 /**
  * Funtion init makes sure that <body> (or function includeHTML) is loaded before the other functions run
@@ -30,13 +14,41 @@ function init(){
 
   includeHTML();
 
-  showAllTaskTitles();
+  loadPage();
 
+}
+
+/**
+ * set the general functions hier to use many times below 
+ * @param {} x id of the element
+ */
+function show(x){
+  document.getElementById(x).style.display = 'flex';
+}
+function hide(x){
+  document.getElementById(x).style.display = 'none';
+}
+
+/**
+ * a general functions that we set hier to use many times below.
+ * this function specifies an object that how it looks like and which index it has in its array.
+ * @param {array} array the array that contains this object.
+ * @param {string} id the id in this object.
+ */
+ function findTaskBy(id){
+  currentTask = allTasks.find(task => task['id'] == id);
+  currentTaskIndex = allTasks.indexOf(currentTask);
+  console.log('allTasks: ', allTasks, 'currentTask:', currentTask, 'currentTaskIndex', currentTaskIndex) 
 }
 
 
 
-/*== HTML TEMPLATES ==========================================*/
+
+
+
+/*======================
+HTML TEMPLATES 
+======================*/
 
 /**
  * This Function is to include HTML snippets in HTML.
@@ -75,166 +87,298 @@ function includeHTML() {
 
 
 
-/*== MAIN ================================================== */
-
-let category, title, description, deadline;
-
-let today = new Date();
-
-let date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
-
-let allTasks = [];
-
-function setToLocalStorage(key, array) { //set here and use below
-  localStorage.setItem(key, JSON.stringify(array));
-}
-function getFromLocalStorage(key) { //set here and use below
-  return JSON.parse(localStorage.getItem(key));
-}
 
 
-if(getFromLocalStorage('allTasks')){
-  allTasks = getFromLocalStorage('allTasks');
+
+/*==========================
+MAIN
+========================== */
+
+
+
+/**
+ * this is the second function that run automaticaly when the page is opened.
+ * 
+ */
+function loadPage(){
+  renderAllColumns();
+  renderAddAColumn();
 }
 
-function showAllTaskTitles(){
-  let todoList = document.getElementById('todoList');
-  todoList.innerHTML = ``;
-  allTasks.forEach((item, i) =>{
-    todoList.innerHTML += `
-      <div id="task${i}" class="card" draggable="true" onmouseover="showFunctionBtn(${i})" onmouseout="hideFunctionBtn(${i})">
-        <button onclick="saveEditedTask(${i})" id="saveEditedTask${i}" class="saveEditedTaskBtn btn btn-light btn-sm">Save</button>
-        <div class="card-header bg-transparent border-light d-flex justify-content-between align-items-center">  
-          <input id="title${i}" value="${allTasks[i].title}" readonly onclick="editTask(this, ${i})">
+
+
+
+
+
+
+
+/**======================================
+ALL FUNCTIONS FOR COLUMNS
+========================================*/
+
+
+
+/**
+ * show on board all columns with their names and their tasks
+ */
+ function renderAllColumns(){
+  let board = document.getElementById('board');
+  board.innerHTML = '';
+  allColumns.forEach((column, i) => {
+    board.innerHTML += `
+      <div class="column card" id="column${column.id}" onmouseover="show('delColumn${column.id}')" onmouseout="hide('delColumn${column.id}')">
+        <button id="delColumn${column.id}" onclick="delColumn(${column.id}, ${i})" class="btn btn-close btn-sm border" style="display:none;"></button>
+        
+        <div class="card-body">
+            <h5 class="card-title">${column.columnName}</h5>
+            <div class="list" ondrop="dropTo('todoList')" ondragover="allowDrop(event)"></div>
         </div>
-        <div class="collapse" id="detail${i}">
-          <div class="card-body">
-            <textarea id="description${i}" type="text" oninput="this.style.height = '';this.style.height = this.scrollHeight + 'px';" readonly  onclick="editTask(this, ${i})">${allTasks[i].description}</textarea>
-            
-            <div class="d-flex align-items-center">
-              <span >Deadline</span>
-              <input id="deadline${i}" type="datetime-local" value="${allTasks[i].deadline}" readonly  onclick="editTask(this, ${i})">
-            </div>
-          </div>
-          <div class="card-footer bg-transparent border-light">
-              <small class="text-muted d-flex justify-content-between">
-                <div onclick="showSelectCategory(this, ${i})">${allTasks[i].category}</div>
-                <select class="collapse" id="category${i}" class="mt-1 mb-2" style="display:none;">
-                  <option selected>${allTasks[i].category}</option>
-                  <option value="Client">Client</option>
-                  <option value="Product">Product</option>
-                  <option value="Management">Management</option>
-                </select>
-              </small>
-              <small class="text-muted d-flex justify-content-between">
-                <i>Created at ${allTasks[i].createdAt}</i>
-              </small>
-          </div>
-        </div>
-        <div class="function-btn" id="functionBtn${i}" style="display:none;">
-          <button onclick="reverseChevron(this)" class="btn btn-light btn-sm" data-bs-toggle="collapse" data-bs-target="#detail${i}">
-            <i class="fas fa-chevron-down"></i>
-          </button>
-          <button id="delete${i}" onclick="delTask(${i})" class="btn btn-light btn-sm">⨉</button>
-        </div>
+
+        <div class="card-footer"></div>
       </div>
     `;
+    renderAddATask(column.id);
+    renderAllTasks_onlyTitle(column);
   })
 }
 
-function openTaskInputField(){
-  document.getElementById('addATask').style.display = 'none';
-  document.getElementById('addTitleField').style.display = 'block';
-  document.getElementById('addTitleField').style.backgroundColor = 'white';
+
+
+/**
+ * show a button names Add A Column on the right the columns
+ */
+function renderAddAColumn(){
+  let board = document.getElementById('board');
+  board.innerHTML += `
+      <div class="card column" onclick="openInputField_addAColumn(this)">
+          <div class="card-body">
+
+              <div class="card-text" id="addAColumn_mark">+ Add a column</div>
+
+              <form id="addAColumn_input" style="display: none;">
+                  <input class="form-control" placeholder="add a column name...">
+                  <div class="mt-2">
+                      <button onclick="nextNewColumn()" class="btn btn-primary btn-sm">Next new column</button>
+                      <button onclick="cancelNewColumn()" class="btn btn-close btn-sm"></button>
+                  </div>
+              </form>
+
+          </div>
+      </div>
+  `;
 }
 
 
 
-function showAFullAddingField(){
-  document.getElementById('showAFullAddingField').style.display = 'none';
-  document.getElementById('fullAddingField').style.display = 'block';
-}
-function hideFullAddingField(){
-  document.getElementById('showAFullAddingField').style.display = 'flex';
-  document.getElementById('fullAddingField').style.display = 'none';
+function openInputField_addAColumn(thisInputField){
+
+  show('addAColumn_input');
+  hide('addAColumn_mark');
+
+  console.log(thisInputField);
+  // thisInputField.onclick = null;
+
+  document.addEventListener('click', function(e){
+    if(!thisInputField.contains(e.target)){
+      saveNewColumn();
+      loadPage();
+    }
+  })
+
 }
 
 
-function save() {
+
+/**
+ * on the field Add A Column, after filling a column name,
+ * click on button Next New Column to
+ * create a new column with title and button Add A Task
+ * and open the input field Add A Column by side to continue create other column.
+ */
+function nextNewColumn(){
+  saveNewColumn();
+  loadPage();
+  openInputField_addAColumn();
+}
+
+
+
+function saveNewColumn(){
+  columnName = document.querySelector('#addAColumn_input > input').value;
+  column = {
+    'columnName': columnName,
+    'id': new Date().getTime(),
+  }
+  allColumns.push(column);
+}
+
+
+
+function cancelNewColumn(){
+  hide('addAColumn_input');
+  show('addAColumn_mark');
+}
+
+
+
+function delColumn(columnID, i){
+  currentColumn = allColumns.find(column => column.id == columnID)
+  //Del Column:
+  allColumns.splice(i, 1, currentColumn);
+
+  //Del all tasks of this column:
+  // let taskOfThisColumn = allTasks.find(task => task.columnID == columnID);
+  // console.log(taskOfThisColumn)
+  // while (taskOfThisColumn) {
+  //   taskIndex = allTasks.indexOf(taskOfThisColumn)
+  //   allTasks.splice(taskIndex, 1, taskOfThisColumn)
+  //   taskOfThisColumn = allTasks.find(task => task.columnID == columnID);
+  // }
+}
+
+
+
+
+
+
+
+
+/**===========================
+ALL FUNCTIONS FOR TASKS
+============================ */
+
+
+
+/**
+ * show the tasks on the specified column
+ */
+function renderAllTasks_onlyTitle(column){
+
+  tasksOfThisColumn = allTasks.filter(task => task['columnID'] == column.id);
+  console.log('allTasks: ', allTasks, 'tasksOfThisColumn:', tasksOfThisColumn) 
+
+  let listTasks = document.getElementById(`column${column.id}`).querySelector('.list');
+
+  listTasks.innerHTML = ``;
+  if(tasksOfThisColumn){
+    tasksOfThisColumn.forEach(task =>{
+      listTasks.innerHTML += generateTasks(task);
+    })
+  }
+
+}
+
+
+
+function generateTasks(task){
+  return `
+    <div id="task${task.id}" class="card" draggable="true" onmouseover="show('functionBtn${task.id}')" onmouseout="hide('functionBtn${task.id}')">
+
+      <button onclick="saveEditedTask(${task.id})" id="saveEditedTask${task.id}" class="saveEditedTaskBtn btn btn-light btn-sm">Save</button>
+      <div class="function-btn" id="functionBtn${task.id}" style="display:none;">
+        <button onclick="reverseChevron(this)" class="btn btn-light btn-sm" data-bs-toggle="collapse" data-bs-target="#detail${task.id}">
+          <i class="fas fa-chevron-down"></i>
+        </button>
+        <button id="delete${task.id}" onclick="delTask(${task.id})" class="btn btn-light btn-sm">⨉</button>
+      </div>
+
+      <div class="card-header bg-transparent border-light d-flex justify-content-between align-items-center">  
+        <textarea readonly id="title${task.id}" onclick="editTask(this, ${task.id})" oninput="resizeWith(this)">${task.title}</textarea>
+      </div>
+
+      <div class="collapse" id="detail${task.id}">
+        <div class="card-body">
+
+          <textarea readonly id="description${task.id}" type="text" oninput="resizeWith(this)" rows="4" onclick="editTask(this, ${task.id})">${task.description}</textarea>
+          
+          <div class="d-flex align-items-center">
+            <i>Deadline</i>
+            <input readonly id="deadline${task.id}" class="deadline" type="datetime-local" value="${task.deadline}"  onclick="editTask(this, ${task.id})">
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  `;
+}
+
+
+/**
+ * resizes the input field like the size of the input content.
+ * @param {HTML element} thisInputField the current Input Field that's being worked on.
+ */
+function resizeWith(thisInputField){
+  thisInputField.style.height = '';
+  thisInputField.style.height = thisInputField.scrollHeight + 'px';
+}
+
+
+
+
+/**
+ * show a button names Add A Task on the end of each column
+ * @param {object} column from array allColumns
+ */
+function renderAddATask(columnID){
+  document.getElementById(`column${columnID}`).querySelector('.card-footer').innerHTML = `
+      <div class="card-text" onclick="openInputField_addATask(${columnID})">+ Add a task</div>
+  `; 
+}
+
+
+
+
+
+/**
+ * open a field to create a new task by filling a title
+ */
+function openInputField_addATask(columnID){
+  document.getElementById(`column${columnID}`).querySelector('.card-footer').innerHTML = `
+    <div id="addTitleField" class="addingField">
+        <textarea id="title" placeholder="add a title..."
+            oninput="this.style.height = '';this.style.height = this.scrollHeight + 'px';"></textarea>
+        
+        <div id="fullAddingField" style="display:none;">
+            <textarea id="description" placeholder="add a description..."
+                oninput="this.style.height = '';this.style.height = this.scrollHeight + 'px';"></textarea>
+            
+            <div class="d-flex align-items-center">
+                <i>Deadline</i>
+                <input id="deadline" class="deadline" type="datetime-local">
+            </div>
+        </div>
+
+        <div class="task-buttons">
+            <button id="showAFullAddingField" onclick="showAFullAddingField()" class="btn btn-light btn-sm">Add a full task</button>
+            <button onclick="saveNewTask(${columnID})" class="btn btn-outline-dark btn-sm">Save</button>
+            <button onclick="renderAddATask(${columnID})" class="btn btn-close"></button>
+        </div>
+    </div>
+  `;
+}
+
+
+
+
+function saveNewTask(columnID) {
   
-  category = document.getElementById('category').value;
   title = document.getElementById('title').value;
   description = document.getElementById('description').value;
   deadline = document.getElementById('deadline').value;
   let task = {
-    'category': category,
-    'title': title.replaceAll('\n', `<br>`),
-    'description': description.replaceAll('\n', `<br>`),
+    'id': new Date().getTime(),
+    'columnID': columnID,
+    'title': title,
+    'description': description,
     'deadline': deadline,
-    'createdAt': date,
   };
   
   allTasks.push(task);
-  setToLocalStorage('allTasks', allTasks);
-  showAllTaskTitles();
-  backToAddTitleField();
+
+  currentColumn = allColumns.find(column => column.id == columnID) //specifies this column
+
+  renderAllTasks_onlyTitle(currentColumn) //render all tasks of this column
 
 }
 
-
-
-function backToAddTitleField(){
-  hideFullAddingField();
-  document.getElementById('addTitleField').style.display = 'none';
-  document.getElementById('addATask').style.display = 'block';
-}
-
-
-
-function showFunctionBtn(i){
-  document.getElementById(`functionBtn${i}`).style.display = 'flex';
-}
-function hideFunctionBtn(i){
-  document.getElementById(`functionBtn${i}`).style.display = 'none';
-}
-
-function reverseChevron(thisBtn){
-  thisBtn.querySelector('i').classList.toggle('fa-chevron-up')
-}
-
-function editTask(editField, i){
-  document.getElementById(`saveEditedTask${i}`).style.display ="block";
-  editField.readOnly = false;
-}
-
-function showSelectCategory(thisCategory, i){
-  document.getElementById(`saveEditedTask${i}`).style = "display: block;";
-  thisCategory.style = "display:none;"
-  document.getElementById(`category${i}`).style = "display: flex;"
-}
-
-function saveEditedTask(i) {
-  
-  document.getElementById(`saveEditedTask${i}`).style = "display: none;";
-  let editedCategory = document.getElementById(`category${i}`).value;
-  let editedTitle = document.getElementById(`title${i}`).value;
-  let editedDescription = document.getElementById(`description${i}`).value;
-  let editedDeadline = document.getElementById(`deadline${i}`).value;
-  let editedTask = {
-    'category': editedCategory,
-    'title': editedTitle.replaceAll('\n', `<br>`),
-    'description': editedDescription.replaceAll('\n', `<br>`),
-    'deadline': editedDeadline,
-    'createdAt': date,
-  };
-  
-  allTasks.splice(i, 1, editedTask);
-  setToLocalStorage('allTasks', allTasks);
-  showAllTaskTitles();
-
-}
-function delTask(i){
-  allTasks.splice(i, 1);
-  setToLocalStorage('allTasks', allTasks);
-  showAllTaskTitles();
-}
